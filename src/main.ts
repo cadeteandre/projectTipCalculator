f18e396eca5dfd2495094985e458ccadeb019b53
Andre Cadete <andrecadete70@gmail.com>
2024-10-24 00:41:35 +0200

    first commit

---
+++ src/main.ts
@@ -0,0 +1,36 @@
+const button = document.querySelector('button') as HTMLButtonElement;
+const showResults = document.querySelector('#showResults') as HTMLElement;
+
+const tipCalculator = (billValue: string, peopleAmount: string, serviceQuality: string) => {
+    const result = {
+        billValue: Number(billValue),
+        peopleAmount: Number(peopleAmount),
+        serviceQuality: serviceQuality,
+        tip: 0,
+        pricePerPerson: 0
+    }
+    switch(true) {
+        case (result.serviceQuality === 'bad'): 
+            result.tip = result.billValue * 0.02
+            break;
+        case (result.serviceQuality === 'ok'):
+            result.tip = result.billValue * 0.1
+            break;
+        case (result.serviceQuality === 'super'):
+            result.tip = result.billValue * 0.2
+            break;
+    }
+    result.pricePerPerson = (result.billValue + result.tip) / result.peopleAmount;
+
+    return result;
+}
+
+button.addEventListener('click', () => {
+    const inputBill = document.querySelector('#inputBill') as HTMLInputElement;
+    const inputPersonNum = document.querySelector('#inputPersonNum') as HTMLInputElement;
+    const serviceQuality = document.querySelector('#qualityService') as HTMLSelectElement;
+
+    const result = tipCalculator(inputBill.value, inputPersonNum.value, serviceQuality.value);
+
+    showResults.innerHTML = `<p>${result.tip.toFixed(2)} / ${(result.billValue + result.tip).toFixed(2)} / ${result.pricePerPerson.toFixed(2)}</p>`;
+})
